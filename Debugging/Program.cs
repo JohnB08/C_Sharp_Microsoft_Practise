6b69b22ef38942c6c21d76356fe7e1d3fd171fa3
JohnB08 <142892183+JohnB08@users.noreply.github.com>
2024-04-25 13:34:33 +0200

    mhm

---
+++ Debugging/Program.cs
@@ -0,0 +1,22 @@
+﻿var value = 10;
+
+int ValueAccepter(int value)
+{
+    return value++;
+}
+
+int value2 = ValueAccepter(value);
+
+int y = 5;
+void PrintValue()
+{
+    int y = 10;
+    Console.WriteLine(y);
+}
+PrintValue();
+
+Console.WriteLine($"{!(true && false)}");
+
+
+string myString = "Hello";
+string mySecondString = "World!";
